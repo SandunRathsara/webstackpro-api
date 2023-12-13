264cef1dd462426970f0cb4d4a7b6f4020573529
Sandun Rathsara <sandun@surge.global>
2023-12-13 12:15:27 +0530

    API integrated to get todos and mark them as completed with optimistic updates

--- src/utils/date-time-formats.ts
+++ src/utils/date-time-formats.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon';
 
 export function formatDateTime(date: Date): string {
-  return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_SHORT);
+  return DateTime.fromJSDate(date).toLocaleString(
+    DateTime.DATE_MED_WITH_WEEKDAY,
+  );
 }
